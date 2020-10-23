120fb496bfac982898393bbcc380b3586a275616
bidulgicode <31906555+MikumiX@users.noreply.github.com>
2020-10-23 12:43:23 +0900

    Unity 2D - 스킬 시스템, 평타
    1. CreatureController에 스킬처리를 위해 코드 수정
    2. 캐릭터가 바라보는 방향에 위치하는 적 판별 가능

--- Client/Assets/Scripts/Controllers/CreatureController.cs
+++ Client/Assets/Scripts/Controllers/CreatureController.cs
@@ -56,6 +56,32 @@ public class CreatureController : MonoBehaviour
         }
     }
 
+    // 내가 바라보는 방향의 바로 앞 Cell 포지션 (타격처리용)
+    // 범위공격인 경우?
+    public Vector3Int GetFrontCellPos(int range = 1)
+    {
+        Vector3Int cellPos = CellPos;
+
+        // 내가 타격직전 마지막으로 바라보는곳 기준으로
+        switch (_lastDir)
+        {
+            case MoveDir.Up:
+                cellPos += Vector3Int.up * range;
+                break;
+            case MoveDir.Down:
+                cellPos += Vector3Int.down * range;
+                break;
+            case MoveDir.Left:
+                cellPos += Vector3Int.left * range;
+                break;
+            case MoveDir.Right:
+                cellPos += Vector3Int.right * range;
+                break;
+        }
+
+        return cellPos;
+    }
+
     // 상태에 따른 애니메이션을 자동으로 호출한다. 지금은 state와 dir의 영향을 받음
     protected virtual void UpdateAnimation()
     {
@@ -109,7 +135,26 @@ public class CreatureController : MonoBehaviour
         }
         else if (_state == CreatureState.Skill)
         {
-            // TODO
+            // 내가 스킬쓰기 직전까지 바라보고 있던 방향으로 스킬이 시전되어야 한다
+            switch (_lastDir) 
+            {
+                case MoveDir.Up:
+                    _animator.Play("ATTACK_BACK");
+                    _sprite.flipX = false;
+                    break;
+                case MoveDir.Down:
+                    _animator.Play("ATTACK_FRONT");
+                    _sprite.flipX = false;
+                    break;
+                case MoveDir.Left:
+                    _animator.Play("ATTACK_RIGHT");
+                    _sprite.flipX = true; // 스프라이트 x축 반전 (오->왼)
+                    break;
+                case MoveDir.Right:
+                    _animator.Play("ATTACK_RIGHT");
+                    _sprite.flipX = false;
+                    break;
+            }
         }
         else
         {
@@ -145,42 +190,32 @@ public class CreatureController : MonoBehaviour
     // Update에 똑같은거 때려넣지 말고 필요하면 UpdateController()를 재정의
     protected virtual void UpdateController()
     {
-        UpdatePosition(); // 이미 이동한 상태지만 부드럽게 이동하는것처럼 보여주기 위한
-        UpdateIsMoving(); // 입력기준으로 이동
-    }
-
-    // 스르륵 이동 처리
-    private void UpdatePosition()
-    {
-        if (State != CreatureState.Moving)
-            return;
-
-        // 서버기준으로는 이미 _cellPos에 이동해있는 상황임
-        Vector3 destPos = Managers.Map.CurrentGrid.CellToWorld(CellPos) + new Vector3(0.5f, 0.5f);
-        Vector3 moveDir = destPos - transform.position; // 방향벡터 구하기
-
-        // 도착 여부 체크
-        float dist = moveDir.magnitude; // 목적지까지의 남은 거리
-        if (dist < _speed * Time.deltaTime) // 1틱에 이동 가능한 거리보다 더 적게 남았으면 도착처리
+        // State 에 따라 어떤 컨트롤러들을 Update 해줄건지 나눔
+        // UpdatePosition()의 경우 Moving 상태일떄만 들어오길 원하니
+        // UpdatePosition 내부에  if (State != CreatureState.Moving) 같은 코드가 있는데 이것을 대체
+        // State별로 전담하는 Update 함수가 달라지므로 책임이 명확해진다
+        switch (State)
         {
-            // 도착
-            transform.position = destPos;
-            // 예외적으로 애니메이션을 직접 컨트롤
-            // 키를 계속 누르고 있는 경우에도 계속 Idle 애니메이션이 재생되는 문제때문에 이렇게 함
-            _state = CreatureState.Idle; // _state에 직접 넣으면 UpdateAnimation 호출을 안함
-            if (_dir == MoveDir.None) // 이동키에서 정말로 손을 땠다 (멈춤)
-                UpdateAnimation(); // Idle 애니메이션을 틀어줘
-        }
-        else
-        {
-            // 아직 가는 중
-            transform.position += moveDir.normalized * _speed * Time.deltaTime;
-            State = CreatureState.Moving;
+            case CreatureState.Idle:
+                UpdateIdle(); // 입력기준으로 이동
+                break;
+            case CreatureState.Moving:
+                UpdateMoving(); // 이미 이동한 상태지만 부드럽게 이동하는것처럼 보여주기 위한
+                break;
+            case CreatureState.Skill:
+                UpdateSkill();
+                break;
+            case CreatureState.Dead:
+                UpdateDead();
+                break;
+            default:
+                break;
         }
     }
 
     // 이동 가능한 상태일 때, 실제로 이동
-    private void UpdateIsMoving()
+    // 재정의 가능하게 바꿈
+    protected virtual void UpdateIdle()
     {
         // 움직이지 않을때만
         if (State == CreatureState.Idle && _dir != MoveDir.None)
@@ -214,12 +249,53 @@ public class CreatureController : MonoBehaviour
                 // ObjectManager의 Find()를 CanGo() 안에서 호출하느냐
                 // 아니면 CanGo()를 호출한대서 같이 호출하느냐 문제가 있는데
                 // Find()가 변경될 여지가 있으므로 CanGo()와 분리시켜 놓는게 좋다고 생각
-                if(Managers.Object.Find(destPos) == null)
+                if (Managers.Object.Find(destPos) == null)
                 {
                     // 충돌 날 물체가 없다.
                     CellPos = destPos;
                 }
             }
         }
+    }
+
+    // 스르륵 이동하는 것처럼 보이게 하는 코드
+    protected virtual void UpdateMoving()
+    {
+        // 내가 원하는 상태(직전)만 받음
+        //if (State != CreatureState.Moving)
+        //    return;
+
+        // 서버기준으로는 이미 _cellPos에 이동해있는 상황임
+        Vector3 destPos = Managers.Map.CurrentGrid.CellToWorld(CellPos) + new Vector3(0.5f, 0.5f);
+        Vector3 moveDir = destPos - transform.position; // 방향벡터 구하기
+
+        // 도착 여부 체크
+        float dist = moveDir.magnitude; // 목적지까지의 남은 거리
+        if (dist < _speed * Time.deltaTime) // 1틱에 이동 가능한 거리보다 더 적게 남았으면 도착처리
+        {
+            // 도착
+            transform.position = destPos;
+            // 예외적으로 애니메이션을 직접 컨트롤
+            // 키를 계속 누르고 있는 경우에도 계속 Idle 애니메이션이 재생되는 문제때문에 이렇게 함
+            _state = CreatureState.Idle; // _state에 직접 넣으면 UpdateAnimation 호출을 안함
+            if (_dir == MoveDir.None) // 이동키에서 정말로 손을 땠다 (멈춤)
+                UpdateAnimation(); // Idle 애니메이션을 틀어줘
+        }
+        else
+        {
+            // 아직 가는 중
+            transform.position += moveDir.normalized * _speed * Time.deltaTime;
+            State = CreatureState.Moving;
+        }
+    }
+
+    protected virtual void UpdateSkill()
+    {
+
+    }
+
+    protected virtual void UpdateDead()
+    {
+
     }
 }
--- Client/Assets/Scripts/Controllers/PlayerController.cs
+++ Client/Assets/Scripts/Controllers/PlayerController.cs
@@ -6,6 +6,7 @@ using static Define;
 
 public class PlayerController : CreatureController
 {
+    Coroutine _coSkill; // 스킬 시전중에 State 변화 막을 용도
     // CreatureController의 Start와 Update가 자동으로 호출됨
     protected override void Init()
     {
@@ -14,9 +15,21 @@ public class PlayerController : CreatureController
 
     protected override void UpdateController()
     {
-        GetDirInput();
+        // 스킬을 사용중이면 방향전환을 막는다던지..
+        switch (State)
+        {
+            case CreatureState.Idle:
+                GetDirInput();
+                GetIdleInput();
+                break;
+            case CreatureState.Moving:
+                GetDirInput();
+                break;
+        }
         base.UpdateController();
     }
+
+
 
     void LateUpdate()
     {
@@ -50,5 +63,37 @@ public class PlayerController : CreatureController
             // 정지
             Dir = MoveDir.None;
         }
+    }
+
+    // 스킬 전담 클래스가 따로 있는게 낫다.
+    // 스킬 사용중에는 상태변화를 막고 싶은데 시간 기준으로 한다 치면 어떻게 시간을 카운트?
+    // 1. Update()
+    // 2. Coroutine
+    private void GetIdleInput()
+    {
+        // 스킬(임시)
+        // GetDirInput()에 있던 것을 분리
+        if (Input.GetKey(KeyCode.Space))
+        {
+            State = CreatureState.Skill; // UpdateAnimation은 알아서 불러줄거임
+            // 0.5초 후에 스킬시전상태(CreatureState.Skill)를 푼다
+            _coSkill = StartCoroutine("CoStartPunch"); 
+        }
+    }
+
+    IEnumerator CoStartPunch()
+    {
+        // 피격 판정
+        // 내가 바라보고 있는 방향 기준 바로 앞의 좌표
+        GameObject go = Managers.Object.Find(GetFrontCellPos());
+        if (go != null)
+        {
+            Debug.Log(go.name);
+        }
+
+        // 대기 시간
+        yield return new WaitForSeconds(0.5f);
+        State = CreatureState.Idle;
+        _coSkill = null;
     }
 }
